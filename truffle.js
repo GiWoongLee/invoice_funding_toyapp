f3bd5a2c0d88d1eeb0ad9a89a04ce0ccf2ebb530
woong <wooong1225@gmail.com>
2018-05-08 15:57:33 -0700

    Config truffle.js

--- truffle.js
+++ truffle.js
@@ -1,4 +1,14 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    networks : {
+      development : {
+        hosts : "127.0.0.1",
+        port : 8545,
+        network_id : "*" // match any network id
+      },
+      crowdz : {
+        hosts : "18.144.59.112",
+        port : 8545,
+        network_id : ""
+      }
+    }
 };
