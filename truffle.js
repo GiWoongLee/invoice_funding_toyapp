6ad936276f93dff0d0713c95cd0275c7247ce3b6
woong <wooong1225@gmail.com>
2018-05-08 17:10:22 -0700

    [Bug] debug config of turffle.js

--- truffle.js
+++ truffle.js
@@ -1,14 +1,15 @@
 module.exports = {
-    networks : {
-      development : {
-        hosts : "127.0.0.1",
-        port : 8545,
-        network_id : "*" // match any network id
-      },
-      crowdz : {
-        hosts : "18.144.59.112",
-        port : 8545,
-        network_id : ""
-      }
+  networks : {
+    development : {
+      host : "127.0.0.1",
+      port : 8545,
+      network_id : "*" // match any network id
+    },
+    crowdz : {
+      host : "18.144.59.112",
+      port : 8545,
+      network_id : "" // 
     }
-};
+  }
+}
+
